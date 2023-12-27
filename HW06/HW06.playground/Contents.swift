17ccbc53e8bb463eb6314cee5a94f698afa37402
Kirill Golovlev <k-ork-a@mail.ru>
2023-12-27 19:27:20 +0300

    Sdelal 6-e DZ

--- HW06/HW06.playground/Contents.swift
+++ HW06/HW06.playground/Contents.swift
@@ -1,8 +1,38 @@
-//В рамках прошлого дз был создан класс пиццерии с переменной, в которой хранится пицца, удалите ее. Необходимо создать структуру картошки фри, в которой будет стоимость и размер картошки и сделать так, чтобы в классе пиццерии была одна переменная, в которую можно было бы класть и пиццу, и картошку фри.
-//Добавьте в класс пиццерии функцию, которая будет добавлять новую позицию в меню.
-//Создайте протокол, в котором будут содержаться функции открытия и закрытия.
-//Написать расширение для класса пиццерии, в котором будет реализован протокол из пункта 3.
+//Создать структуру работника пиццерии. В ней должны быть такие свойства, как имя, зарплата и должность. Должностей пока может быть две: или кассир, или повар. Добавить в класс пиццерии массив с работниками
+struct Employee {
+    var name: String
+    var salary: Int
+    var workPosition: WorkPosition
+    
+    enum WorkPosition {
+        case coocker
+        case cashier
+    }
+}
 
+
+//Создать класс столика, в нем должны быть свойство, в котором содержится количество мест и функция, которая на вход принимает количество гостей, которое хотят посадить, а возвращает true, если места хватает и false, если места не хватает. Изначальное количество мест задается в инициализаторе
+
+class Table {
+    var seats: Int
+    
+    func isAvalibleSeats(guests: Int) -> Bool {
+        if (guests <= seats) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // создаем слабую ссылку
+    weak var pizzeria: Pizzeria?
+    init(pizzeria: Pizzeria, seats: Int) {
+        self.seats = seats
+    }
+}
+
+
+//Добавить в класс пиццерии свойство, в котором хранится массив столиков. У класса столика добавить свойство, в котором хранится пиццерия, в которой стоит столик. Столики создаются сразу в инициализаторе, не передаются туда в качестве параметра
 
 
 struct Pizza: MenuItem {
@@ -33,11 +63,19 @@ struct FriPotato: MenuItem {
 
 
 class Pizzeria {
+    var employees: [Employee]
     var menuItems: [MenuItem]
+    var tables: [Table] = [] //Здесь создаем пустой масив "столов" чтобы в инциализаторе инциализировать то колличество столов, которое необходимо
     
-    init(menuItems: [MenuItem]) {
-           self.menuItems = menuItems
-       }
+    init(employees: [Employee], menuItems: [MenuItem], numOfTables: Int) {
+        self.employees = employees
+        self.menuItems = menuItems
+        
+      for _ in 1...numOfTables {
+            tables.append(Table(pizzeria: self, seats: 3))
+        }
+    }
+    
     func addNewMenuItem(menuNewItem: MenuItem) {
         menuItems.append(menuNewItem)
     }
